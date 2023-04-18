28f37b2e303a84b524f9593d0e107c55c504d140
Pavlo <p1kutsaev@gmail.com>
2023-04-18 21:25:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={store}>
-     
-    <App />
-    
-    </Provider>
+         <App />
+        </Provider>
   </React.StrictMode>
 );
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,45 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { combineReducers } from '@reduxjs/toolkit';
 
 import { filterSlice } from './filterSlice';
 import { contactsSlice } from './contactsSlice';
-
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-
-// const rootReducer = combineReducers({
-//   contacts: contactsSlice.reducer,
-//   filter: filterSlice.reducer,
-// });
-
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-//   whitelist: ['contacts'],
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
 
 export const store = configureStore({
   reducer: {
@@ -47,7 +9,3 @@ export const store = configureStore({
     filter: filterSlice.reducer,
   },
 });
-
-// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену "contacts/fetchAll".
-// addContact - додавання контакту (метод POST). Базовий тип екшену "contacts/addContact".
-// deleteContact - видалення контакту (метод DELETE). Базовий тип екшену "contacts/deleteContact".
